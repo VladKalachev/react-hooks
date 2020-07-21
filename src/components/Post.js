3ec8dbab1bc734f1452e594d58a761e7e266de4b
Corvax <vlvete1990@yandex.ru>
2020-07-21 18:40:43 +0300

    chore: add useEffect

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,13 +1,15 @@
 import React from 'react'
 
-function Post({title, body}) {
+function Post({title, name, email, body}) {
   return(
     <div className="article-content">
     <div className="article-title">
         <a href="/">{title}</a>
+        <a href="/">{name}</a>
     </div>
       <p className="article-text">
           {body}
+          {email}
       </p>
     </div>
   ) 
