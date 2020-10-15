5c1d5ffa51219ac77b8f80a399ca4f385e95f56d
Corvax <vlvete1990@yandex.ru>
2020-10-15 15:53:29 +0300

    chore: add useMemo and React.memo

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,18 +1,27 @@
-import React from 'react'
+import React, {useState, useMemo} from 'react'
+
+function sum(n) {
+  return n + n
+}
 
 function Post({title, name, email, body}) {
+  const [num, setNum] = useState(0)
+  const [isGreen, setIsGreen] = useState(false)
+  const countSum = useMemo(() => sum(num), [num])
   return(
     <div className="article-content">
     <div className="article-title">
         <a href="/">{title}</a>
         <a href="/">{name}</a>
     </div>
-      <p className="article-text">
+      <p className="article-text" style={{ color: isGreen ? 'green' : 'red' }} onClick={() => setIsGreen(!isGreen)}>
           {body}
           {email}
       </p>
+      {countSum}
+      <button onClick={() => setNum(num + 1)} style={{ marginLeft: '10px' }}>+</button>
     </div>
   ) 
 }
 
-export default Post
+export default React.memo(Post)
