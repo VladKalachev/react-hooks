131a6f27f668adf9d313c7babbc49818900eb200
Corvax <vlvete1990@yandex.ru>
2020-10-16 20:33:55 +0300

    chore: add useRef

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,20 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 import Header from './Header'
 import PostList from './PostList'
 import Footer from './Footer'
 
 function App() {
-
+  const ref = useRef(null)
   const [posts, setPost] = useState([])
   const [theme, setTheme] = useState('light')
   const [check, setCheck] = useState(false)
   const [type, setType] = useState('posts')
-
+  
   useEffect(() => {
     setTimeout(() => {
       document.title = `Page ${type}`
     }, 300)
-   
+  
     fetch(`https://jsonplaceholder.typicode.com/${type}`)
       .then(response => response.json())
       .then(json => {
@@ -30,8 +30,16 @@ function App() {
     setCheck(!check)
   }
 
+  const handleFocus = () => {
+    ref.current.style.color = 'red'
+  }
+
   return (
     <div className={`app ${theme}`}>
+      <div className="form">
+        <input ref={ref} />
+        <button onClick={handleFocus}>focus</button>
+      </div>
       <Header changeType={setType} check={check} changeTheme={change}/>
       <PostList posts={posts}/>
       <Footer />
