627247afc179780d9a6606ca5b14ca3f41543b36
Corvax <vlvete1990@yandex.ru>
2020-12-19 19:35:37 +0300

    chore: add useReducer

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,76 @@
-import React, {useState, useEffect, useRef, useContext} from 'react'
+import React, {useEffect, useRef, useContext, useReducer} from 'react'
 import Header from './Header'
 import PostList from './PostList'
 import Footer from './Footer'
 import ThemeContext from '../context'
 
+function init(state) {
+  return {...state }
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'fetch': 
+      return {
+        ...state,
+        posts: action.payload
+      }
+
+    case 'posts': 
+      return {
+        ...state,
+        type: action.type
+      }
+
+    case 'users': 
+      return {
+        ...state,
+        type: action.type
+      }
+
+    case 'check': 
+      return {
+        ...state,
+        check: !state.check
+      }
+    case 'reset':
+      return init(action.payload);
+  
+    default:
+      return state;
+  }
+}
+
 function App() {
   const ref = useRef(null)
-  const [posts, setPost] = useState([])
-  const [check, setCheck] = useState(false)
-  const [type, setType] = useState('posts')
+  const [data, dispatch] = useReducer(reducer, { posts: [], check: false, type: 'posts' }, init)
   const {theme, setTheme} = useContext(ThemeContext)
   
   useEffect(() => {
     setTimeout(() => {
-      document.title = `Page ${type}`
+      document.title = `Page ${data.type}`
     }, 300)
   
-    fetch(`https://jsonplaceholder.typicode.com/${type}`)
+    fetch(`https://jsonplaceholder.typicode.com/${data.type}`)
       .then(response => response.json())
       .then(json => {
-        setPost(json)
+        dispatch({ type: "fetch", payload: json })
       })
       return () => {
         document.title = "Page"
       }
-  }, [type])
+  }, [data.type])
 
   const change = () => {
     setTheme(theme === 'light' ? 'dark' : 'light')
-    setCheck(!check)
+    dispatch({ type: "check" })
   }
 
   const handleFocus = () => {
     ref.current.style.color = 'red'
+  }
+  const setType = (type) => {
+    dispatch({ type })
   }
 
   return (
@@ -41,8 +79,8 @@ function App() {
         <input ref={ref} />
         <button onClick={handleFocus}>focus</button>
       </div>
-      <Header changeType={setType} check={check} changeTheme={change}/>
-      <PostList posts={posts}/>
+      <Header changeType={setType} check={data.check} changeTheme={change}/>
+      <PostList posts={data.posts}/>
       <Footer />
     </div>
   )
